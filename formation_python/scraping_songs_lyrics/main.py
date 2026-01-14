72fcb485f80666f0bb7d9f8d52e606db75ff94ce
ABIJURU Honoré Julien <abijul001@gmail.com>
2026-01-14 12:24:05 +0100

    adding scraping_songs_lyrics project

---
+++ formation_python/scraping_songs_lyrics/main.py
@@ -0,0 +1,79 @@
+from pprint import pprint
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def get_all_lyrics_urls() -> list[str]:
+    LYRICS_WEBSITE_URL = "https://genius.com"
+    next_page = '1'
+    page = next_page
+    lyrics_urls = []
+
+    while next_page != None:
+        r = requests.get(f"https://genius.com/api/artists/22451/songs?page={page}&sort=popularity")
+        response = r.json().get("response")
+        # pprint(response)
+        songs = response.get("songs")
+        if(len(songs) > 0):
+            # pprint(songs)
+            lyrics_paths = []
+            for song in songs:
+                lyrics_paths.append(song["path"])
+            # pprint(lyrics_paths)
+            lyrics_urls.extend([LYRICS_WEBSITE_URL+path for path in lyrics_paths])
+            # pprint(lyrics_urls)
+        next_page = response.get("next_page")
+        try:
+            page = next_page
+        except:
+            print("Page not found")
+
+    pprint(lyrics_urls)
+    print(len(lyrics_urls))
+
+    return lyrics_urls
+
+def get_words_from_all_lyrics():
+
+    lyrics_urls = get_all_lyrics_urls()
+
+    for url in lyrics_urls:
+        html = requests.get(url).text
+
+        soup = BeautifulSoup(html, "html.parser")
+
+        lyrics_divs = soup.find_all(
+            "div", attrs={"data-lyrics-container": "true"}
+        )
+        # pprint(lyrics_divs)
+
+        # for div in lyrics_divs:
+        #     for tag in div.select(
+        #             "a[data-lyrics-annotation-id], span.ReferentFragment"
+        #     ):
+        #             pprint(tag)
+        #             tag.unwrap()
+        #             tag.decompose()
+
+        for div in lyrics_divs:
+            # Remove annotation links
+            for tag in div.find_all(["a", "span"], attrs={
+                "data-lyrics-annotation-id": True
+            }):
+                pprint("=== pre :\n {tag}")
+                # tag.unwrap()
+                tag.decompose()
+                pprint("=== post :\n {tag}")
+
+        lyrics = "\n".join(
+            div.get_text(separator="\n", strip=True)
+            for div in lyrics_divs
+        )
+
+        lyrics = re.sub(r"\[.*?\](?:\s*\(x\s*\d+\))?\n?", "", lyrics).strip()
+
+        # print(lyrics)
+
+# get_all_lyrics_urls()
+get_words_from_all_lyrics()
